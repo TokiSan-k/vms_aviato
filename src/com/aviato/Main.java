84d672a43d93e1a5d5f084408d775354bc3cb497
dev1654 <104884276+dev1654@users.noreply.github.com>
2025-03-26 16:30:32 -0400

    Added initial styling to Customer.fxml page

--- src/com/aviato/Main.java
+++ src/com/aviato/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 
@@ -11,6 +12,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        // Load the Maven Pro font
+        try {
+            Font regularFont = Font.loadFont(getClass().getResourceAsStream("/fonts/MavenPro-Regular.ttf"), 14);
+            Font mediumFont = Font.loadFont(getClass().getResourceAsStream("/fonts/MavenPro-Medium.ttf"), 14);
+            Font boldFont = Font.loadFont(getClass().getResourceAsStream("/fonts/MavenPro-Bold.ttf"), 14);
+            System.out.println("Maven Pro font loaded successfully.");
+            // Print the font family names
+            System.out.println("Regular Font Family: " + regularFont.getFamily());
+            System.out.println("Medium Font Family: " + mediumFont.getFamily());
+            System.out.println("Bold Font Family: " + boldFont.getFamily());
+        } catch (Exception e) {
+            System.err.println("Failed to load Maven Pro font: " + e.getMessage());
+        }
+
         // Load the FXML file
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/pages/Customer.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -22,7 +37,6 @@ public class Main extends Application {
         primaryStage.setMinHeight(400.0);
         primaryStage.setMaximized(true);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
